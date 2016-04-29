46d2d4f0fd55da2912dbcbf20fea4a84354c67c7
Brian Woodward <brian.woodward@gmail.com>
2016-04-29 02:06:03 -0400

    implementation

--- index.js
+++ index.js
@@ -7,15 +7,92 @@
 
 'use strict';
 
-var debug = require('debug')('set-getter');
+var toPath = require('to-object-path');
 
-module.exports = function(config) {
-  return function(app) {
-    if (this.isRegistered('set-getter')) return;
+/**
+ * Defines a getter function on an object using property path notation.
+ *
+ * ```js
+ * var obj = {};
+ *
+ * // root level property
+ * getter(obj, 'foo', function() {
+ *   return 'bar';
+ * });
+ * // obj.foo === 'bar'
+ *
+ * // property dot notation
+ * getter(obj, 'bar.baz', function() {
+ *   return 'qux';
+ * });
+ * // obj.bar.baz === 'qux'
+ *
+ * // property array notation
+ * getter(obj, ['beep', 'boop'], function() {
+ *   return 'bop';
+ * });
+ * // obj.beep.boop === 'bop'
+ * ```
+ * @param {Object} `obj` Object to add property to.
+ * @param {String|Array} `prop` Property string or array to add.
+ * @param {Function} `getter` Getter function to add as a property.
+ * @api public
+ */
 
-    this.define('set-getter', function() {
-      debug('running set-getter');
-      
-    });
-  };
-};
+function setGetter(obj, prop, getter) {
+  var key = toPath(arguments);
+  return define(obj, key, getter);
+}
+
+/**
+ * Define getter function on object or object hierarchy using dot notation.
+ *
+ * @param  {Object} `obj` Object to define getter property on.
+ * @param  {String} `prop` Property string to define.
+ * @param  {Function} `getter` Getter function to define.
+ * @return {Object} Returns original object.
+ */
+
+function define(obj, prop, getter) {
+  if (!~prop.indexOf('.')) {
+    defineProperty(obj, prop, getter);
+    return obj;
+  }
+
+  var keys = prop.split('.');
+  var last = keys.pop();
+  var target = obj;
+  var key;
+
+  while ((key = keys.shift())) {
+    while (key.slice(-1) === '\\') {
+      key = key.slice(0, -1) + '.' + keys.shift();
+    }
+    target = target[key] || (target[key] = {});
+  }
+
+  defineProperty(target, last, getter);
+  return obj;
+}
+
+/**
+ * Define getter function on object as a configurable and enumerable property.
+ *
+ * @param  {Object} `obj` Object to define property on.
+ * @param  {String} `prop` Property to define.
+ * @param  {Function} `getter` Getter function to define.
+ */
+
+function defineProperty(obj, prop, getter) {
+  Object.defineProperty(obj, prop, {
+    configurable: true,
+    enumerable: true,
+    get: getter
+  });
+}
+
+/**
+ * Expose `setGetter`
+ */
+
+module.exports = setGetter;
